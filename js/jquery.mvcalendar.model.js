1a3abf7e040b8b34163783db7079570629267490
Colin Waddell <mrcolin@gmail.com>
2013-06-20 00:33:39 +0100

    basic model prototype in place

--- js/jquery.mvcalendar.model.js
+++ js/jquery.mvcalendar.model.js
@@ -1,18 +1,32 @@
 (function ($) {
-  var pluginName = 'calendar';
-  var extensionMethods = {
-        /*
-         * retrieve the id of the element
-         * this is some context within the existing plugin
-         */
-    showId: function(){
-        return this.element[0].id;
-        //console.log('hi');
+  /**
+   *  Namespace: the namespace the plugin is located under
+   *  pluginName: the name of the plugin
+   */
+  var calendarModel = {
+    /*
+     * retrieve the id of the element
+     * this is some context within the existing plugin
+     */
+    model: {
+
+
+    },
+
+    // Called after main plugin has completed _create
+    _init: function () {
+      $.Model('Todo', {
+        findAll: 'GET /todos.json',
+        findOne: 'GET /todos/{id}.json',
+        create: 'POST /todos.json',
+        update: 'PUT /todos/{id}.json',
+        destroy: 'DELETE /todos/{id}.json'
+      }, {});
     }
+
   };
 
-  $.extend(true, $.fn[pluginName], extensionMethods);
+  $.extend(true, $.bwtw.calendar.prototype, calendarModel);
 
 
 })(jQuery);
-
